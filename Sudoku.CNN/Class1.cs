d849bcda25d6ac11098ff2b0b8eac90ae865e75e
Matthias NGUEND NJIKI <matthias.nguend-njiki@edu.ece.fr>
2021-01-15 09:43:59 +0100

    Ajout Neuron Solver by Matthias NGUEND

---
+++ Sudoku.CNN/Class1.cs
@@ -0,0 +1,17 @@
+﻿using Sudoku.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sudoku.CNN
+{
+    public class CNNsolver : ISudokuSolver
+    {
+        public void Solve(GrilleSudoku s)
+        {
+            //cstart coding
+            //transform the sud pour le reseau de neur
+
+        }
+    }
+}
---
+++ Sudoku.CNN/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sudoku.CNN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
