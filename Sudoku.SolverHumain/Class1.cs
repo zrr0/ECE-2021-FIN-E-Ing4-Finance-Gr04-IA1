9be697903a42686a8e8f0f2df3f1d2b85d0f91c9
Jérémy ITZKOWITCH <jeremy.itzkowitch@edu.ece.fr>
2021-01-15 10:30:38 +0100

    ajout solver externe

---
+++ Sudoku.SolverHumain/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Sudoku.Core;
+using System;
+
+namespace Sudoku.SolverHumain
+{
+    public class SolverHumain : ISudokuSolver
+    {
+        public void Solve(GrilleSudoku s)
+        {
+            //           
+        }
+    }
+}
