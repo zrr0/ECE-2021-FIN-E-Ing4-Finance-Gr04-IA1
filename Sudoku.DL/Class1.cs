5612ae39303fcc3032df649a2c14ba8fb8041a30
Louis Loisel <louis.loisel@icloud.com>
2021-01-22 09:07:09 +0100

    Initialisation SudokuDL

---
+++ Sudoku.DL/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Sudoku.Core;
+using System;
+
+namespace Sudoku.DL
+{
+    public class DLSolver : ISudokuSolver
+    {
+        public void Solve(GrilleSudoku s)
+        {
+            
+        }
+    }
+}
