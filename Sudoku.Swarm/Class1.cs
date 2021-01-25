6108dc3ac427b7c0e0a660adb29f81760f221874
Giglobastre <kenny.huber@edu.Ece.fr>
2021-01-25 14:52:41 +0100

    link les 2

---
+++ Sudoku.Swarm/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using SudokuCombinatorialEvolutionSolver;
+using System.Linq;
+
+namespace Sudoku.Swarm
+{
+    public class Solver : Core.ISudokuSolver
+    {
+        public void Solve(Core.GrilleSudoku s)
+        {
+            Console.WriteLine(s.ToString());
+
+            var sudoku = SudokuCombinatorialEvolutionSolver.Sudoku.Difficult;
+
+            var solver = new SudokuSolver();
+
+            var solvedSudoku = solver.Solve(sudoku,200,5000,40);
+
+            Console.WriteLine(solvedSudoku.ToString());
+
+            //return (solvedSudoku.ToString());
+        }
+    }
+}
