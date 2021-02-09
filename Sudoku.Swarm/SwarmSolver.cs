7e78a3f0b6345bf90966039af33952bc6de51e0d
HugoTeinturier <hugo.teinturier@edu.ece.fr>
2021-02-09 21:51:11 +0100

    Final avant pres
    Optimisation de 2 min à 30 sec pour moyen.
    Fonctionnement Foreeach en commentaire sous le solv dans Sukoku solver

--- Sudoku.Swarm/SwarmSolver.cs
+++ Sudoku.Swarm/SwarmSolver.cs
@@ -21,21 +21,42 @@ namespace Sudoku.Swarm
             
            var solver = new SudokuSolver();
            
-            var solvedSudoku = solver.Solve(sudoku, 200, 5000, 40);
+            var solvedSudoku = solver.Solve(sudoku, 1,1,0);
             // Possibilité d'incrémenter le nombre d'organismes jusqu'à obtenir une solution?
             // Mais les meilleures optimisations à faire sont probablement dans le code du solver lui-même, auquel cas, il faudrait copier le code dans le projet plutôt que le référencer
 
-            var numOrganisms = 200;
-     
+            
+            double numOrganisms = 20;
+            double epochs = 2000;
+            var max_retry = 10;
+            int cnt = 0;
+
             do
             {
-                solvedSudoku = solver.Solve(sudoku, numOrganisms, 5000, 1);
-                numOrganisms *= 2;
-            } while (solvedSudoku.Error>0);
+                solvedSudoku = solver.Solve(sudoku, (int)numOrganisms, (int)epochs, max_retry);
+                if (cnt == 0)
+                {
+                    numOrganisms = 200;
+                    // Console.WriteLine ($"nb orga =: {numOrganisms}");
+                    max_retry = 300;
+                }
+                else if (cnt >= 0)
+                {
+                    //  Console.WriteLine($"nb orga =: {numOrganisms}");
+
+                    numOrganisms *= 1.5;
+                    if (max_retry < 300)
+                    {
+                        max_retry += 50;
+                    }
+                }
+                cnt++;
+            } while (solvedSudoku.Error > 0);
 
             //affichage du sudoku
             Console.WriteLine(solvedSudoku.ToString());
             //Reconversion depuis int[,]
+            //Console.WriteLine($"nb restars=: {max_retry}");
             for (int row = 0; row < 9; row++)
             {
                 for (int column = 0; column < 9; column++)
